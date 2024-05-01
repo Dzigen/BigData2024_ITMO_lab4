867d473d575ee01ce4f6c371285c66cadbf05e31
dizgen <dzigen@gmail.com>
2024-05-01 17:08:21 +0300

    re-structuring preject folder

---
+++ mongodb/mongo-init.js
@@ -0,0 +1,14 @@
+db.createUser({
+    user: 'dzigen',
+    pwd: 'password',
+    roles: [
+        {
+            role: 'readWrite',
+            db: 'modelapi_log'
+        }
+    ]
+});
+
+db = new Mongo().getDB('modelapi_log');
+
+db.createCollection('saved_requests', {capped: false});
